e10aba116f46268646cb1fc27b163660904d65f2
Prashant Pokhriyal <prashant.pokhriyal@vedantu.com>
2021-03-14 19:29:05 +0530

    remove query parameter when modal closes

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -5,13 +5,17 @@ export default function MovieDetails(props) {
     const directors = props.details?.credits?.crew
         ?.filter(person => person.job === 'Director');
     const rating = 5 * (props.details.vote_average / 10);
+    const closeModal = () => {
+        window.history.pushState(null, '', `?`);
+        props.setShowModal(false);
+    };
     return (
         <>
             {props.showModal ? (
                 <>
                     <div
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                        onClick={() => props.setShowModal(false)}
+                        onClick={closeModal}
                     >
                         <div className="relative w-auto my-6 mx-auto max-w-4xl">
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -71,7 +75,7 @@ export default function MovieDetails(props) {
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
                                         type="button"
                                         style={{ transition: "all .15s ease" }}
-                                        onClick={() => props.setShowModal(false)}
+                                        onClick={closeModal}
                                     >
                                         Close
                                     </button>
